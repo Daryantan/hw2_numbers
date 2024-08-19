b40f2b4ef95e0c6c8f729db8c81c70e957dc6364
Daria <dasha.sheludko03@gmail.com>
2024-08-19 12:19:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# hw_2.1
+
+print("Please, enter 4-significant number: ")
+number = int(input())
+print(str(number))
+n1=number//1000
+n2=number//100%10
+n3=number%100//10
+n4=number%10
+print(n1)
+print(n2)
+print(n3)
+print(n4)
