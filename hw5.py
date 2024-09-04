d17ae7fc567187094a27d1aa06a14ec6dff8fe86
Daria <dasha.sheludko03@gmail.com>
2024-09-04 14:44:53 +0300

    Added task 5.2

--- hw5.py
+++ hw5.py
@@ -6,29 +6,40 @@
 # Тобто, потрібно робити запит до користувача на продовження роботи калькулятора після кожного обчислення -
 # якщо користувач ввів yes (можна просто y), то нове обчислення, інакше - закінчення роботи.
 
-# print("Please, enter first number: ")
-# a = int(input())
-# print("Please, enter second number: ")
-# b = int(input())
-# print("Please, enter number of action : ")
-# print(" 1) Addition \n 2) Deletion \n 3) Multiplication \n 4) Division")
-# action = int(input())
-# match action:
-#     case 1:
-#         print(a+b)
-#     case 2:
-#         print(a-b)
-#     case 3:
-#         print(a*b)
-#     case 4:
-#         if b == 0:
-#             print("Ouups! Division by zero is impossible!")
-#         else:
-#             print(a/b)
-#     case _:
-#         print("Invalid option, please, try again")
+while True:
+    print("Do you want to continue?")
+    print("Please, enter Yes, if you want, or No to exit.")
+    answer = input().lower()
 
+    if answer == 'yes' or answer == 'y':
+        print("Please, enter first number: ")
+        a = int(input())
+        print("Please, enter second number: ")
+        b = int(input())
+        print("Please, enter number of action: ")
+        print(" 1) Addition \n 2) Subtraction \n 3) Multiplication \n 4) Division")
+        action = int(input())
 
+        if action == 1:
+            print("Result:", a + b)
+        elif action == 2:
+            print("Result:", a - b)
+        elif action == 3:
+            print("Result:", a * b)
+        elif action == 4:
+            if b == 0:
+                print("Oops! Division by zero is impossible!")
+            else:
+                print("Result:", a / b)
+        else:
+            print("Invalid option, please, try again.")
+
+    elif answer == 'no' or answer == 'n':
+        print("Exiting the calculator. Goodbye!")
+        break
+
+    else:
+        print("Invalid option, please, try again.")
 
 # hw_5.3
 # Користувач вводить рядок, Ваше завдання – перетворити рядок на hashtag.
@@ -43,21 +54,21 @@
 # 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
 # 't!e@s#t t%e^s&t' => #TestTest
 
-#my_str = "Python Community"
-my_str = "i like python community!"
-#my_str = "Should, I. subscribe? Yes!"
-#my_str = "t!e@s#t t%e^s&t"
-nstr = ''
-my_str = my_str.title()
-
-
-i = 0
-for let in my_str:
-    if let !='!' and let !=',' and let !='@' and let !='.' and let !='?' and let !='#' and let !='^' and let !='%' and let !='&' and let !=' ':
-        nstr = nstr + let
-        i += 1
-if len(nstr)<140:
-    print("#" + nstr, sep='')
-else:
-    len(nstr)//2
-    print("#" + nstr, sep='')
+# #my_str = "Python Community"
+# my_str = "i like python community!"
+# #my_str = "Should, I. subscribe? Yes!"
+# #my_str = "t!e@s#t t%e^s&t"
+# nstr = ''
+# my_str = my_str.title()
+#
+#
+# i = 0
+# for let in my_str:
+#     if let !='!' and let !=',' and let !='@' and let !='.' and let !='?' and let !='#' and let !='^' and let !='%' and let !='&' and let !=' ':
+#         nstr = nstr + let
+#         i += 1
+# if len(nstr)<140:
+#     print("#" + nstr, sep='')
+# else:
+#     len(nstr)//2
+#     print("#" + nstr, sep='')
