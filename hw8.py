f834b1fa1148da58abbc19ea19893e43f81f7337
Daria <dasha.sheludko03@gmail.com>
2024-09-12 13:55:46 +0300

    Added task 8.3

--- hw8.py
+++ hw8.py
@@ -47,14 +47,41 @@
 # assert is_palindrome('a.') == True, 'Test3'
 # assert is_palindrome('aurora') == False, 'Test4'
 # print("ОК")
-def is_palindrome(text):
-    ntext = ''.join(let.lower() for let in text if let.isalnum())
-    ntext_2 = ntext[::-1]
-    return ntext == ntext_2
+# def is_palindrome(text):
+#     ntext = ''.join(let.lower() for let in text if let.isalnum())
+#     ntext_2 = ntext[::-1]
+#     return ntext == ntext_2
+#
+#
+# assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+# assert is_palindrome('0P') == False, 'Test2'
+# assert is_palindrome('a.') == True, 'Test3'
+# assert is_palindrome('aurora') == False, 'Test4'
+# print("ОК")
 
 
-assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-assert is_palindrome('0P') == False, 'Test2'
-assert is_palindrome('a.') == True, 'Test3'
-assert is_palindrome('aurora') == False, 'Test4'
+# hw_8.3
+# Унікальне число
+# Вам необхідно написати функцію find_unique_value, яка приймає список із чисел, знаходить серед них унікальне число та
+# повертати його. Унікальне число - це число, яке зустрічається в списку один раз.
+# Випадок, коли в одному списку буде кілька унікальних чисел, перевіряти не потрібно.
+# Приклад:
+# def find_unique_value(some_list):
+#    pass
+# assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+# assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+# assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+# print("ОК")
+
+def find_unique_value(some_list):
+   unique = int
+   for number in some_list:
+      if some_list.count(number) == 1:
+         unique = number
+
+   return unique
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 print("ОК")
