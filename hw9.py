9371309a079f867870a1fac76e67d908ce9ab38f
Daria <dasha.sheludko03@gmail.com>
2024-09-13 23:02:34 +0300

    Added task 9.2

--- hw9.py
+++ hw9.py
@@ -16,15 +16,45 @@
 # assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK')
 #
 
-def popular_words (text, words):
-    text_lower = text.lower().split()
-    wrds = {}
-    '''
-    створюємо словник, щоб зберігати у нього результат, після чого в циклі підраховуємо кількість слів
-    '''
-    for word in words:
-        wrds[word] = text_lower.count(word)
-    return wrds
+# def popular_words (text, words):
+#     text_lower = text.lower().split()
+#     wrds = {}
+#     '''
+#     створюємо словник, щоб зберігати у нього результат, після чого в циклі підраховуємо кількість слів
+#     '''
+#     for word in words:
+#         wrds[word] = text_lower.count(word)
+#     return wrds
+#
+# assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+# print('OK')
 
-assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+
+
+# hw_9.2
+# Різниця між числами
+# Є набір чисел (float або int). Вам потрібно знайти різницю між найбільшим (максимум) і найменшим (мінімум) елементом.
+# Ваша функція difference має вміти працювати з невизначеною кількістю аргументів.
+# Якщо аргументів немає, то функція повертає 0 (нуль).
+# Якщо з 3-м тестом будуть проблеми, використовуйте функцію округлення round(x, 2),
+# де х це число, яке потрібно округлити.
+# Вх. Дані: Змінна кількість аргументів як числа (int, float).
+# Вих. Дані: Різниця між максимумом і мінімумом як число (int, float).
+# Приклади:
+# assert difference(1, 2, 3) == 2, 'Test1'
+# assert difference(5, -5) == 10, 'Test2'
+# assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+# assert difference() == 0, 'Test4'
+# print('OK')
+
+def difference(*args):
+    if not args:
+        return 0
+    res = round(max(args) - min(args), 2)
+    return res
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
 print('OK')
