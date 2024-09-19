8e189984625e094c00ac880a3b713f37ae7187df
Daria <dasha.sheludko03@gmail.com>
2024-09-20 01:28:27 +0300

    Added task 10.3

--- hw10.py
+++ hw10.py
@@ -76,18 +76,44 @@ import re
 # assert first_word("Hello.World") == "Hello", 'Test6'
 # print('OK')
 
-def first_word(text):
-    """ Пошук першого слова """
-    result = re.search(r"[a-zA-Z']+", text)
-    if result:
-        return result.group(0)
-    return
+# def first_word(text):
+#     """ Пошук першого слова """
+#     result = re.search(r"[a-zA-Z']+", text)
+#     if result:
+#         return result.group(0)
+#     return
+#
+#
+# assert first_word("Hello world") == "Hello", 'Test1'
+# assert first_word("greetings, friends") == "greetings", 'Test2'
+# assert first_word("don't touch it") == "don't", 'Test3'
+# assert first_word(".., and so on ...") == "and", 'Test4'
+# assert first_word("hi") == "hi", 'Test5'
+# assert first_word("Hello.World") == "Hello", 'Test6'
+# print('OK')
 
 
-assert first_word("Hello world") == "Hello", 'Test1'
-assert first_word("greetings, friends") == "greetings", 'Test2'
-assert first_word("don't touch it") == "don't", 'Test3'
-assert first_word(".., and so on ...") == "and", 'Test4'
-assert first_word("hi") == "hi", 'Test5'
-assert first_word("Hello.World") == "Hello", 'Test6'
+# hw_10.3
+# Перевірити чи є парним чи ні
+# Ваша функція is_even повинна повертати True якщо число парне, і False якщо число непарне.
+# Вхідні дані: Ціле число.
+# Вихідні дані: Логічний тип.
+# def is_even(digit):
+#     """ Перевірка чи є парним число """
+#     pass
+#
+# assert is_even(2) == True, 'Test1'
+# assert is_even(5) == False, 'Test2'
+# assert is_even(0) == True, 'Test3'
+# print('OK')
+
+def is_even(digit):
+    """ Перевірка чи є парним число """
+    if digit % 2 == 0:
+        return True
+    else: return False
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
 print('OK')
