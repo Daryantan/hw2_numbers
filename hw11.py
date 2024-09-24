845d26562b12f57b3362444127446cfc89f57a77
Daria <dasha.sheludko03@gmail.com>
2024-09-24 16:52:25 +0300

    Added task 11.2

--- hw11.py
+++ hw11.py
@@ -15,26 +15,52 @@
 # assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
 # assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
 # assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+# # print('Ok')
+# def prime_generator(end):
+#     def is_prime(n):
+#         if n < 2:
+#             return False
+#         for i in range(2, int(n ** 0.5) + 1):
+#             if n % i == 0:
+#                 return False
+#         return True
+#
+#     for num in range(2, end + 1):
+#         if is_prime(num):
+#             yield num
+#
+# from inspect import isgenerator
+#
+# gen = prime_generator(1)
+# assert isgenerator(gen) == True, 'Test0'
+# assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+# assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+# assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+#
 # print('Ok')
-def prime_generator(end):
-    def is_prime(n):
-        if n < 2:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
 
-    for num in range(2, end + 1):
-        if is_prime(num):
-            yield num
+
+# # hw_11.2
+# Напишіть функцію-генератор generate_cube_numbers, яка формує набір кубів чисел,
+# починаючи з числа 2 до вказаної Вами величини. Тобто.
+# генератор повинен працювати доти, поки генерується значення менше зазначеної величини.
+# Нагадую, що вийти із генератора можна за допомогою return без параметрів.
+def generate_cube_numbers(end):
+    n = 2
+    while True:
+        cube = n ** 3
+        if cube > end:
+            return
+        yield cube
+        n += 1
 
 from inspect import isgenerator
 
-gen = prime_generator(1)
+gen = generate_cube_numbers(1)
 assert isgenerator(gen) == True, 'Test0'
-assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
-assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
-assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
 
 print('Ok')
+
